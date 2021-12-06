fc4610424d149502526a6edce113b3f883d91645
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2021-12-06 16:27:18 -0500

    Create test data files

---
+++ test/test-data/ingredients-test.js
@@ -0,0 +1,13 @@
+const ingredientsData = [
+  {
+    "id": 20081,
+    "name": "wheat flour",
+    "estimatedCostInCents": 142
+  },
+  {
+    "id": 18372,
+    "name": "bicarbonate of soda",
+    "estimatedCostInCents": 582
+  }
+];
+export default = ingredientsData; 
